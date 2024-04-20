ee0bece80eea1279420e571c7fdcbba702fc906a
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-04-20 23:53:57 +0200

    modified:   lib/api/api_service.dart

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -2,7 +2,8 @@ import 'package:test/api/models.dart';
 import 'package:dio/dio.dart';
 
 final dio = Dio();
-Future<CompanyInfo> getCompanyInfo() async {
+
+Future<CompanyInfo?> getCompanyInfo() async {
   try {
     final response = await dio.get('https://api.spacexdata.com/v4/company');
     if (response.statusCode == 200) {
@@ -15,6 +16,19 @@ Future<CompanyInfo> getCompanyInfo() async {
     throw Exception('Failed to load company info: ${error.message}');
   }
 }
+// Future<CompanyInfo> getCompanyInfo() async {
+//   try {
+//     final response = await dio.get('https://api.spacexdata.com/v4/company');
+//     if (response.statusCode == 200) {
+//       final data = response.data as Map<String, dynamic>;
+//       return CompanyInfo.fromJson(data);
+//     } else {
+//       throw Exception('Failed to load company info: ${response.statusCode}');
+//     }
+//   } on DioError catch (error) {
+//     throw Exception('Failed to load company info: ${error.message}');
+//   }
+// }
 
 // Future<CompanyInfo> getCompanyInfo() async {
 //   try {
@@ -31,8 +45,8 @@ Future<CompanyInfo> getCompanyInfo() async {
 //     }
 //   } on DioError catch (error) {
 //     throw Exception('Failed to load company info: ${error.message}');
-//   }
-// }
+//  }
+ //}
 
 Future<List<Ship>> getShips() async {
   try {
